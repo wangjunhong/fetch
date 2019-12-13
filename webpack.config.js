1a7136b9d6cea4a547f111b2a2a99f4806f651e5
admin <1262485655@qq.com>
2019-12-13 11:40:34 +0800

    目录

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-  entry: ['./index.js'],
+  entry: ['./src/index.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].min.js',
+    filename: 'fetch.min.js',
     libraryTarget: 'umd'
   },
   mode:"development"
